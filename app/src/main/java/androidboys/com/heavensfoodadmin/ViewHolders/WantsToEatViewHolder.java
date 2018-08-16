7a8d818fd80821d5c969bcf8dd1b4cab238c3195
arvind <negiarvind229@gmail.com>
2018-08-16 21:55:42 +0530

    Succesfully added edit and delete function in wantstoeatfragment and specialorderfragment

--- app/src/main/java/androidboys/com/heavensfoodadmin/ViewHolders/WantsToEatViewHolder.java
+++ app/src/main/java/androidboys/com/heavensfoodadmin/ViewHolders/WantsToEatViewHolder.java
@@ -17,16 +17,15 @@ public class WantsToEatViewHolder extends RecyclerView.ViewHolder implements Vie
     public ImageView wantsFoodImageView;
     public TextView wantsFoodNameTextView;
     public TextView wantsFoodDescriptionTextView;
-    public CheckBox wantsFoodCheckBox;
 
     public WantsToEatViewHolder(@NonNull View itemView) {
         super(itemView);
         wantsFoodImageView=itemView.findViewById(R.id.wantsFoodImageView);
         wantsFoodNameTextView=itemView.findViewById(R.id.wantsFoodNameTextView);
         wantsFoodDescriptionTextView=itemView.findViewById(R.id.wantsFoodDescriptionTextView);
-        wantsFoodCheckBox=itemView.findViewById(R.id.wantsFoodCheckBox);
 
         itemView.setOnClickListener(this);
+        itemView.setOnCreateContextMenuListener(this);
     }
 
 
