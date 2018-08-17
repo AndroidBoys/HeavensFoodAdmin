0a24dca52cceed0c53b82b65d1d216c7d70489b1
arvind <negiarvind229@gmail.com>
2018-08-17 18:05:31 +0530

    Succesfully added edit and delete function in planFragment

--- app/src/main/java/androidboys/com/heavensfoodadmin/Adapters/OurPlansCustomArrayAdapter.java
+++ app/src/main/java/androidboys/com/heavensfoodadmin/Adapters/OurPlansCustomArrayAdapter.java
@@ -13,22 +13,27 @@ import java.util.ArrayList;
 
 import androidboys.com.heavensfoodadmin.Activities.DescriptionActivity;
 import androidboys.com.heavensfoodadmin.Fragments.BuySubscriptionFragment;
-import androidboys.com.heavensfoodadmin.Models.OurPlans;
+import androidboys.com.heavensfoodadmin.Models.Plan;
 import androidboys.com.heavensfoodadmin.R;
 import info.hoang8f.widget.FButton;
 
 public class OurPlansCustomArrayAdapter extends ArrayAdapter {
-    private ArrayList<OurPlans> ourPlans;
+    private ArrayList<Plan> ourPlans;
     private DescriptionActivity hostingActivity;
     private ImageView planImageView;
     private TextView planNameTextView;
     private TextView showDetailButton;
 
+    public OurPlansCustomArrayAdapter(DescriptionActivity hostingActivity, ArrayList<Plan> ourPlans) {
+        super(hostingActivity,R.layout.our_plans_fragment_listview_row,ourPlans);
+        this.ourPlans=ourPlans;
+        this.hostingActivity=hostingActivity;
+    }
+
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
         LayoutInflater layoutInflater= (LayoutInflater) hostingActivity.getSystemService(hostingActivity.LAYOUT_INFLATER_SERVICE);
         View view=layoutInflater.inflate(R.layout.our_plans_fragment_listview_row,parent,false);
-        ourPlans=new ArrayList<>();
         planImageView=view.findViewById(R.id.packsImageView);
         planNameTextView=view.findViewById(R.id.packName);
         showDetailButton = (FButton)view.findViewById(R.id.showDetailsButton);
@@ -43,11 +48,20 @@ public class OurPlansCustomArrayAdapter extends ArrayAdapter {
             }
         });
 
-        Picasso.with(hostingActivity).load(ourPlans.get(position).getPackImageUrl()).into(planImageView);
-        planNameTextView.setText(ourPlans.get(position).getPackName());
+        Picasso.with(hostingActivity).load(ourPlans.get(position).getPlanImageUrl()).into(planImageView);
+        planNameTextView.setText(ourPlans.get(position).getPlanName());
+
+        view.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+
+                return false;
+            }
+        });
 
         return view;
     }
+
 
     @Override
     public int getCount()
@@ -55,9 +69,6 @@ public class OurPlansCustomArrayAdapter extends ArrayAdapter {
         return ourPlans.size();
     }
 
-    public OurPlansCustomArrayAdapter(DescriptionActivity hostingActivity, ArrayList<OurPlans> ourPlans) {
-        super(hostingActivity,R.layout.our_plans_fragment_listview_row,ourPlans);
-        this.ourPlans=ourPlans;
-        this.hostingActivity=hostingActivity;
-    }
+
+
 }
