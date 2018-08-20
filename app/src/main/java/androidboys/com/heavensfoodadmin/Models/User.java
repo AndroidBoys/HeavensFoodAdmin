996f31fd3f3eb6536a7dbf37e1bc73936b773e45
Thesohan <sohan.cse16@nituk.ac.in>
2018-08-20 19:42:55 +0530

    don't know what it is

--- app/src/main/java/androidboys/com/heavensfoodadmin/Models/User.java
+++ app/src/main/java/androidboys/com/heavensfoodadmin/Models/User.java
@@ -1,6 +1,8 @@
 package androidboys.com.heavensfoodadmin.Models;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
 
     public String name;
     public String email;
